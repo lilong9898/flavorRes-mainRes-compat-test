7fcf566810aae9869e830ab891a4b89054ed1201
lilong <lilong@zhangyue.com>
2018-02-27 15:46:11 +0800

    修改：加res二级文件夹过滤

--- flavorRes_mainRes_compat_test/config.py
+++ flavorRes_mainRes_compat_test/config.py
@@ -3,14 +3,19 @@
 
 import os;
 
+isTest = True;
+
 # git仓库的完整路径
-GIT_REPO_REAL_PATH = "/home/lilong/iReader_oppo/";
+GIT_REPO_REAL_PATH = "/home/lilong/hooktest/" if isTest else "/home/lilong/iReader_oppo/";
 
 # productFlavor
-PRODUCT_FLAVOR = "Oppo";
+PRODUCT_FLAVOR = "oppo" if isTest else "Oppo";
 
 # main/res目录的完整路径
-DIR_MAIN_RES_REAL_PATH = os.path.join(GIT_REPO_REAL_PATH, "iReader", "src", "main", "res") + os.sep;
+DIR_MAIN_RES_REAL_PATH = os.path.join(GIT_REPO_REAL_PATH, "main") + os.sep if isTest else os.path.join(GIT_REPO_REAL_PATH, "iReader", "src", "main", "res") + os.sep;
 
 # productFlavor/res目录的完整路径
-DIR_PRODUCT_FLAVOR_REAL_PATH = os.path.join(GIT_REPO_REAL_PATH, "iReader", "src", PRODUCT_FLAVOR, "res") + os.sep;
+DIR_PRODUCT_FLAVOR_RES_REAL_PATH = os.path.join(GIT_REPO_REAL_PATH, "oppo") + os.sep if isTest else os.path.join(GIT_REPO_REAL_PATH, "iReader", "src", PRODUCT_FLAVOR, "res") + os.sep;
+
+# res目录下要参与检测的子目录
+RES_SUB_DIR_PATHS = ("layout", ) if isTest else ("layout", );
--- flavorRes_mainRes_compat_test/main.py
+++ flavorRes_mainRes_compat_test/main.py
@@ -21,21 +21,22 @@ listStrStagedFileRealPathsToWarn = [];
 # 获取暂存区中所有main/res文件夹下的文件路径和所有productFlavor/res文件夹下的文件路径
 for diff in repo.index.diff("HEAD"):
     if(diff.change_type == "M"):
-        realResStagedFilePath = os.path.realpath(GIT_REPO_REAL_PATH + os.sep + diff.b_path)
-        if(realResStagedFilePath.startswith(DIR_MAIN_RES_REAL_PATH)):
-            listStrStagedFileRealPathsInMainRes.append(realResStagedFilePath);
-        if(realResStagedFilePath.startswith(DIR_PRODUCT_FLAVOR_REAL_PATH)):
-            listStrStagedFileRealPathsInProductFlavorRes.append(realResStagedFilePath);
+        realResStagedFilePath = os.path.realpath(os.path.join(GIT_REPO_REAL_PATH, diff.b_path));
+        for resSubDirPath in RES_SUB_DIR_PATHS:
+            if(realResStagedFilePath.startswith(os.path.join(DIR_MAIN_RES_REAL_PATH, resSubDirPath))):
+                listStrStagedFileRealPathsInMainRes.append(realResStagedFilePath);
+            if(realResStagedFilePath.startswith(os.path.join(DIR_PRODUCT_FLAVOR_RES_REAL_PATH, resSubDirPath))):
+                listStrStagedFileRealPathsInProductFlavorRes.append(realResStagedFilePath);
 
 # 获取所有productFlavor/res文件夹下的文件路径
-for (path, d, filelist) in os.walk(DIR_PRODUCT_FLAVOR_REAL_PATH):
+for (path, d, filelist) in os.walk(DIR_PRODUCT_FLAVOR_RES_REAL_PATH):
     for fileName in filelist:
         strFileRealPathInProductFlavorRes = os.path.join(path, fileName);
         listStrFileRealPathsInProductFlavorRes.append(strFileRealPathInProductFlavorRes);
 
 # 统计需要警告的文件路径，这些路径在main/res和productFlavor/res中都存在，且在前者中有进入暂存区的修改，在后者中没有
 for strStagedFileRealPathsInMainRes in listStrStagedFileRealPathsInMainRes:
-    if strStagedFileRealPathsInMainRes.replace(DIR_MAIN_RES_REAL_PATH, DIR_PRODUCT_FLAVOR_REAL_PATH) in listStrFileRealPathsInProductFlavorRes and strStagedFileRealPathsInMainRes.replace(DIR_MAIN_RES_REAL_PATH, DIR_PRODUCT_FLAVOR_REAL_PATH) not in listStrStagedFileRealPathsInProductFlavorRes:
+    if strStagedFileRealPathsInMainRes.replace(DIR_MAIN_RES_REAL_PATH, DIR_PRODUCT_FLAVOR_RES_REAL_PATH) in listStrFileRealPathsInProductFlavorRes and strStagedFileRealPathsInMainRes.replace(DIR_MAIN_RES_REAL_PATH, DIR_PRODUCT_FLAVOR_RES_REAL_PATH) not in listStrStagedFileRealPathsInProductFlavorRes:
         listStrStagedFileRealPathsToWarn.append(strStagedFileRealPathsInMainRes);
 
 # 如果没有危险
@@ -43,14 +44,15 @@ if(len(listStrStagedFileRealPathsToWarn) == 0):
     exit(0);
 
 print()
-print("=========================警告：%s/res覆盖main/res不完全============================"%(PRODUCT_FLAVOR));
-print("这些文件在main的res下有进入暂存区的修改，在" + PRODUCT_FLAVOR + "的res下存在，而且没有进入暂存区的修改:");
+print("=========================警告：%s/res覆盖main/res不完全=============================="%(PRODUCT_FLAVOR));
+print("检测的文件夹包括：%s"%(",".join(RES_SUB_DIR_PATHS)));
+print("这些文件在main的res下有进入暂存区的修改，在%s的res下存在，而且没有进入暂存区的修改:"%(PRODUCT_FLAVOR));
 print();
 
 for path in listStrStagedFileRealPathsToWarn:
     print(path);
 
 print();
-print("这会导致" + PRODUCT_FLAVOR + "/res不能正确反映main/res下的修改!");
-print("=================================================================================");
+print("这会导致%s/res不能正确反映main/res下的修改!"%(PRODUCT_FLAVOR));
+print("==========================================================================================");
 print();
